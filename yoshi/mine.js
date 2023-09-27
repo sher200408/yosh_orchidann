cb26b72398920ee9d64b2e8fa42f701c9d1d3bdc
shermuhammad shertayev backend <113122559+sher200408@users.noreply.github.com>
2023-09-27 10:08:43 +0500

    Add files via upload

---
+++ yoshi/mine.js
@@ -0,0 +1,6 @@
+document.querySelector(".but").addEventListener("click",function(){
+    let yil = document.getElementById("inp");
+    let qqqq = 2023-yil.value;
+    document.querySelector("p").innerHTML="sizning yoshingiz mana"+"<br/>"+qqqq;
+    document.querySelector(".but").value ="";
+})
